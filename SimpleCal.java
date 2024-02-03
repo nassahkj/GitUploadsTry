f10b3cbd75e070a3bfae1b72f2440fc04036ce1a
Hassan <nassahkj@gmail.com>
2024-02-03 12:59:55 +0300

    over all calc functionality added

--- SimpleCal.java
+++ SimpleCal.java
@@ -1,1 +1,51 @@
 //simple calculator application with parallel development
+
+import java.util.Scanner;
+
+public class SimpleCal{
+
+    public static void getUserInput(){
+        int choice;
+        Scanner scanInputs = new Scanner(System.in);
+        while (true) {
+           
+            System.out.print("1. Addition\n2. Subtraction\n3. Division\n4.Product\n7.quit\n\nEnter your choice: ");
+            choice = scanInputs.nextInt();
+                     
+            switch(choice){
+                case 1:
+                     //addition();
+                     break;
+                case 2:
+                     //subtract();
+                     break;
+                case 3: 
+                     //division();
+                     break;
+                case 4: 
+                     //product();
+                     break;
+                case 7:
+                System.out.println("....To be handled later...!");
+                break;                          
+                               
+                default:
+                    break;
+                
+                   
+        }
+        
+            
+        }
+       
+      
+
+    }
+
+    public static void main(String[] args){
+        getUserInput();//call the input function 
+
+
+    }
+
+}
